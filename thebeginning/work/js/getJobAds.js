5850b5c02c8e029f9d91256f99e718b71d383191
perui <ulrika.haggqvist@arbetsformedlingen.se>
2019-03-26 12:25:22 +0100

    work

--- thebeginning/work/js/getJobAds.js
+++ thebeginning/work/js/getJobAds.js
@@ -8,7 +8,7 @@ $.ajax({
     },
     contentType: 'application/json; charset=utf-8',
     success: function (result) {
-       console.log(result)
+      /* console.log(result)*/
         $('#8206102Title').text(result.platsannons.annons.annonsrubrik.replace('/', '/ '))
         $('#8206102AnsokSenast').append(result.platsannons.ansokan.sista_ansokningsdag.slice(0,10))
         $('#8206102AntalPlatser').append(result.platsannons.annons.antal_platser)
