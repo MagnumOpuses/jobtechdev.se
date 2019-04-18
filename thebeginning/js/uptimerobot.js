500fa51acca5291cc305de88ebe2e58910b7c01e
Mats Löfstrand <mats.lofstrand@arbetsformedlingen.se>
2019-04-18 09:57:37 +0200

    status is now linked to info about api

--- thebeginning/js/uptimerobot.js
+++ thebeginning/js/uptimerobot.js
@@ -11,13 +11,15 @@ $.each(monitors, function (index, value ) {
 
 
     if (this.friendly_name != 'www.jobtechdev.se' && this.friendly_name != 'jobtechdev.se' && this.friendly_name != 'backend.jobtechdev.se') {
+        let array = this.friendly_name.split(',');
+
 
         $('#apiStatus').append('<div class="col-lg-2 col-sm-12 mx-auto">\n' +
-            '                        <div class="status text-black text-center wow fadeInUp" >\n' +
-            '                            <div>'+this.friendly_name+'</div>\n' +
+            '                        <a href="'+array[0]+'"><div class="status text-black text-center wow fadeInUp" >\n' +
+            '                            <div>'+array[1]+'</div>\n' +
             '                            <i id="status'+this.status+'" class="status'+this.status+' fas fa-circle"></i>\n' +
             '                        </div>\n' +
-            '                   </div>')
+            '                   </div></a>')
 
     }
 });
