139591f96b5c244376ebd3bbedb52013bb225077
lofms <mats.lofstrand@arbetsformedlingen.se>
2020-09-01 10:03:44 +0200

    C:/Program Files/Git/work hide element if there are no ads

--- static/js/ads.js
+++ static/js/ads.js
@@ -3,6 +3,12 @@ $(function () {
 
     if (row.length === 0){
         document.getElementById("afWidgetContainer").hidden = true;
-        document.getElementById("infoJobs").innerText = "";
+        adsElement = document.getElementById("infoJobs")
+        adsElement.innerText = "";
+        if (document.documentElement.lang == "en") {
+            adsElement.innerText = "Unfortunately, there are no vacancies at the moment";
+        }else{
+            adsElement.innerText = "Tyvärr finns det inga lediga tjänster för tillfället";
+        }
     }
 })
